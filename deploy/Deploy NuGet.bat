97dabea9beec9136fde5966a5d2a544c4e4c6c3a
Keith Jackson <keith@ministryotech.co.uk>
2014-11-03 14:19:44 +0000

    Deploy system updates

--- deploy/Deploy NuGet.bat
+++
@@ -1,8 +0,0 @@
-CALL ../../set-nuget-key.bat
-
-del *.nupkg
-
-nuget pack ../Ministry.WebDriver.Extensions/Ministry.WebDriver.Extensions.csproj -Prop Configuration=Release
-nuget push *.nupkg
-
-pause
