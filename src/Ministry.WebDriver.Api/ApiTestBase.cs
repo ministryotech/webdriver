04953ba4be4d05c986345e350a20a2ade93c0f13
Keith Jackson <keith@ministryotech.co.uk>
2017-09-26 18:55:21 +0100

    New base test classes for API testing using HttpClient

---
+++ src/Ministry.WebDriver.Api/ApiTestBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+
+namespace Ministry.WebDriver.Api
+{
+    /// <summary>
+    /// Base class to be inherrited from by all API tests.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+    public abstract class ApiTestBase
+    {
+        #region | Construction |
+
+        /// <summary>
+        /// Creates a test for a specific API.
+        /// </summary>
+        protected ApiTestBase()
+        {
+            ApiClient = new HttpClient();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets the Http Client.
+        /// </summary>
+        /// <example>
+        /// ApiClient.DefaultRequestHeaders.Accept.Clear();
+        /// ApiClient.DefaultRequestHeaders.Accept.Add(
+        /// new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+        /// ApiClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+        /// 
+        /// var stringTask = ApiClient.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+        ///
+        /// var msg = await stringTask;
+        /// Console.Write(msg);
+        /// </example>
+        /// <remarks>
+        /// Simply creates an HttpClient https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
+        /// </remarks>
+        protected HttpClient ApiClient { get; }
+
+    }
+}
---
+++ src/Ministry.WebDriver.Configuration/ConfiguredApiTestBase.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Ministry.WebDriver.Api;
+
+namespace Ministry.WebDriver.Configuration
+{
+    /// <summary>
+    /// Base class to be inherrited from by all tests.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public abstract class ConfiguredApiTestBase : ApiTestBase
+    {
+        private bool configInitialised;
+
+        private IConfigurationRoot configuration;
+
+        /// <summary>
+        /// Gets the configuration data for the application.
+        /// </summary>
+        protected IConfigurationRoot Configuration
+        {
+            get
+            {
+                if (!configInitialised) InitConfiguration();
+                return configuration;
+            }
+        }
+
+        #region | Supporting Methods |
+        
+        private void InitConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("testsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            configuration = builder.Build();
+
+            configInitialised = true;
+        }
+
+        #endregion | Supporting Methods |
+    }
+}
